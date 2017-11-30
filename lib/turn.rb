8903463eb8896f3029d1d92e6fcb80bc946ea4e9
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-30 01:03:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,6 @@
 def display_board(board)
   board = [ " ", " ", " ", " ", " ", " ", " ", " ", " "]
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
